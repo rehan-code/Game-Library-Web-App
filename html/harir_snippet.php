217eb756223c8606799d0bb3747689ac8e6663a1
Harir Al-Rubaye <halrubay@uoguelph.ca>
2024-01-19 13:39:03 -0500

    harir - about me snippet

---
+++ html/harir_snippet.php
@@ -0,0 +1,10 @@
+<?php
+
+$text = "My name is Harir Al - Rubaye, I am a fifth year student in University of Guelph, majoring in Software Engineering.
+my interest in programming started in highschool, when I took a web development course and I really enjoyed it. 
+I am currently working as a co-op student on my 4th term, and I am looking forward to graduate in the fall of 2024.
+";
+
+$text = "<p style='text-align:justify'>$text</p>";
+
+echo $text;
