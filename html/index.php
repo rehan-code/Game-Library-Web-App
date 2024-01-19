539fd858f950a310a269966ac68cb90e016850a1
onavi01 <77210778+onavi01@users.noreply.github.com>
2024-01-18 23:11:33 -0500

    Added about me snippet.

--- html/index.php
+++ html/index.php
@@ -3,6 +3,6 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Bye cruel World</p>'; ?>
+    <?php echo '<p>Hi there! My name is Ivan Odiel Magtangob. I’m a fifth-year Software Engineering (Co-op) student attending the University of Guelph. A hobbyist musician and video game lover, I fell in love with the art of programming back in high school and have since used code as a medium to express my creativity through a variety of personal projects.</p>'; ?>
   </body>
 </html>
