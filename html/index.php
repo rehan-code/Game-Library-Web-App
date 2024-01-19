19f5291c40bf2848db70eb93b4388cd26de4dfec
onavi01 <77210778+onavi01@users.noreply.github.com>
2024-01-18 23:37:09 -0500

    Pulled sprint-1 changes + added About Us snippet

--- html/index.php
+++ html/index.php
@@ -1,8 +1,64 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <title>PHP Test</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CIS 4250 - Team 8 </title>
+    <link rel="stylesheet" href="style.css" />
   </head>
   <body>
-    <?php echo '<p>Hi there! My name is Ivan Odiel Magtangob. I’m a fifth-year Software Engineering (Co-op) student attending the University of Guelph. A hobbyist musician and video game lover, I fell in love with the art of programming back in high school and have since used code as a medium to express my creativity through a variety of personal projects.</p>'; ?>
+    <header class="container header">
+    <!-- Navbar -->
+      <nav class="nav">
+        <div class="logo">
+          <h2>CIS 4250</h2>
+        </div>
+
+        <div class="nav_menu" id="nav_menu">
+          <button class="close_btn" id="close_btn">
+            <i class="ri-close-fill"></i>
+          </button>
+            <? $pagename = basename($_SERVER['PHP_SELF']);?>
+          <ul class="nav_menu_list">
+            <li class="nav_menu_item">
+              <a <?php if($pagename=="test.php") {echo 'class="current"';}?> href="test.php" class="nav_menu_link">About Us</a>
+            </li>
+          </ul>
+        </div>
+
+        <button class="toggle_btn" id="toggle_btn">
+          <i class="ri-menu-line"></i>
+        </button>
+      </nav>
+    </header>
+
+    <section class="wrapper">
+      <div class="container">
+        <div class="grid-cols-2">
+          <div class="grid-item-1">
+            <h1 class="main-heading">
+              Welcome to <span>Team 8</span>
+              <br />
+              website
+            </h1>
+            <p class="info-text">
+              This is our landing Page for our CIS 4250 Site. Click below to learn more...
+            </p>
+
+            <div class="btn_wrapper">
+              <button class="btn view_more_btn" onclick="">
+                Learn More <i class="ri-arrow-right-line"></i>
+              </button>
+            </div>
+          </div>
+          <div class="grid-item-2">
+            <div class="team_img_wrapper">
+              <img src="https://raw.githubusercontent.com/ananikets18/Responsive-landing-page-using-HTML-CSS-JS-/1b56418e1b24d1f997134084e9214d78a9f91780/img/team.svg" alt="team-img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </body>
 </html>
