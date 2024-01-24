f722a2757ed869106bc679c761217fe0ccd230ad
Harir <57466601+Harir99@users.noreply.github.com>
2024-01-24 11:06:41 -0500

    Frontend for wheres waldo and landing page
    landing page, added wheres waldo game (frontend)

---
+++ html/waldo_game.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Where's Waldo Game</title>
+        <link rel="stylesheet" href="styles/waldo_game.css" />
+    </head>
+    <body>
+    <header>
+        <h1>Where's Waldo Game</h1>
+    </header>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="#">Instructions</a>
+        <a href="about_us/about_us.php">About</a>
+    </nav>
+
+    <div class="image-container" onclick="zoomIn(event)">
+         <img src="http://4.bp.blogspot.com/-NjohvYgHUQU/Ttcs_ommPRI/AAAAAAAABA0/wm6M6HxbI28/s1600/Play+Wheres+Waldo+Online+Puzzle+Game+Department+Store+Shopping+Mall.jpg" draggable="false" alt="Zoomable Image">
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
