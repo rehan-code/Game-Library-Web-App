d3a3e3860c74bb539039f10dd3c38c0c7c37f2fb
onavi01 <77210778+onavi01@users.noreply.github.com>
2024-02-02 21:50:11 -0500

    Added new CodeSniffer ruleset + fixed index.php

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ build:
 	docker compose -f server.docker-compose.yml up -d
 down:
 	docker compose -f server.docker-compose.yml down
+lint:
+	php codesniffer/phpcs.phar -s --standard=./codesniffer/phpcs_ruleset.xml html
--- html/index.php
+++ html/index.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Blah blah blah
+ * php version 8.1.2
+ * 
+ * @author Rehan Nagoor Muhmannanaa <rnahoorm@uoguelph.ca>
+ */
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,11 +15,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>CIS 4250 Team 8</title>
     <style>
-        <?php include 'style.css'; ?>
+        <?php require 'style.css'; ?>
     </style>
 </head>
 <body>
-    <?php include ("components/navbar/navbar.php"); ?>
+    <?php require "components/navbar/navbar.php"; ?>
 
     <section class="wrapper">
         <div class="container">
@@ -22,15 +31,21 @@
                 website
             </h1>
             <p class="info-text">
-                We, Team 8, are dedicated to developing engaging online games. Our inaugural release is an expanded edition of the classic 
-                'Where's Waldo?' game. In this immersive experience, your challenge goes beyond locating Waldo himself, as you discover 
-                a myriad of additional objectives. As our team continues to work diligently, we envision a future where you can explore a 
+                We, Team 8, are dedicated to developing engaging online games.
+                Our inaugural release is an expanded edition of the classic 
+                'Where's Waldo?' game. In this immersive experience, your
+                challenge goes beyond locating Waldo himself, as you discover 
+                a myriad of additional objectives. As our team continues to
+                work diligently, we envision a future where you can explore a 
                 diverse range of games crafted by our dedicated team.
             </p>
             </div>
             <div class="grid-item-2">
             <div class="team-img-wrapper">
-                <img src="https://raw.githubusercontent.com/ananikets18/Responsive-landing-page-using-HTML-CSS-JS-/1b56418e1b24d1f997134084e9214d78a9f91780/img/team.svg" alt="team-img" />
+                <img
+                    src="https://raw.githubusercontent.com/ananikets18/Responsive-landing-page-using-HTML-CSS-JS-/1b56418e1b24d1f997134084e9214d78a9f91780/img/team.svg"
+                    alt="team-img"
+                />
             </div>
             </div>
         </div>
@@ -42,7 +57,10 @@
             <h1 class="main-heading">
                 <span>Our Games</span>
             </h1>
-            <div class="movie-card movie-card-2" onclick="window.location.href='waldo_game/waldo_select.php';">
+            <div
+                class="movie-card movie-card-2"
+                onclick="window.location.href='waldo_game/waldo_select.php';"
+            >
                 <h1 class="main-heading">
                     Where's Waldo Game
                 </h1>
