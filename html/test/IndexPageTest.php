2235d15d59dacc6d35a3218a66926e0f9709345c
onavi01 <77210778+onavi01@users.noreply.github.com>
2024-02-04 15:13:31 -0500

    Linted new frontend tests

--- html/test/IndexPageTest.php
+++ html/test/IndexPageTest.php
@@ -1,33 +1,53 @@
 <?php
+/**
+ * Index page test cases
+ * php version 8.1.2
+ * Authors: Rehan Nagoor Mohideen, Ivan Odiel Magtangob, Harir Al-Rubaye,
+ *          Harikrishan Singh, Nour Tayem, Thulasi Jothiravi
+ */
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Front end tests for the home page
+ */
 class IndexPageTest extends TestCase
 {
-    private $indexPhpContent;
+    private $_indexPhpContent;
 
+    /**
+     * Loads content of home page
+     */
     protected function setUp(): void
     {
         parent::setUp();
-        $this->indexPhpContent = file_get_contents(__DIR__ . '/../index.php');
+        $this->_indexPhpContent = file_get_contents(__DIR__ . '/../index.php');
     }
 
-    // Test if dependent CSS files exist
+    /**
+     * Test if dependent CSS files exist
+     */
     public function testCssFileExists()
     {
         $this->assertFileExists(__DIR__ . '/../style.css');
     }
 
-    // Verify the index.php file itself exists (sanity check)
+    /**
+     * Verify the index.php file itself exists (sanity check)
+     */
     public function testPHPFileExists()
     {
         $this->assertFileExists(__DIR__ . '/../index.php');
     }
 
-    // Verify the index page includes the required navbar component
+    /**
+     * Verify the index page includes the required navbar component
+     */
     public function testPageIncludesNavbar()
     {
-        $this->assertStringContainsString('navbar/navbar.php', $this->indexPhpContent);
+        $this->assertStringContainsString(
+            'navbar/navbar.php', $this->_indexPhpContent
+        );
     }
 
 }
