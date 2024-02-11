2f4109cd2e97bfb1bd3a7dada83e2e595ebbfe4b
your_github_username <59294422+thulasi127@users.noreply.github.com>
2024-02-10 20:36:04 -0500

    Adding unit tests for sprint 4

---
+++ html/test/WaldoStage1Test.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class WaldoStage1Test extends TestCase
+{
+    /**
+     * Checks if the CSS file for the Waldo game exists.
+     */
+    public function testCssFileExists()
+    {
+        $this->assertFileExists(__DIR__ . '/../waldo_game/stages/waldo_game.css');
+    }
+
+    /**
+     * Checks if the navbar component exists.
+     */
+    public function testNavbarComponentExists()
+    {
+        $this->assertFileExists(__DIR__ . '/../components/navbar/navbar.php');
+    }
+
+    /**
+     * Checks if the JavaScript file for the Waldo game exists.
+     */
+    public function testJsFileExists()
+    {
+        $this->assertFileExists(__DIR__ . '/../waldo_game/stages/waldo_game.js');
+    }
+
+    /**
+     * Checks if the image for Waldo Stage 1 exists.
+     */
+    public function testWaldoStage1ImageExists()
+    {
+        $this->assertFileExists(__DIR__ . '/../images/waldo/waldo_stage_1.jpg');
+    }
+}
+
+?>
