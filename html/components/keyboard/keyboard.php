060e7b044d8b2bdfe15e1a96f25dd42ff489706e
onavi01 <77210778+onavi01@users.noreply.github.com>
2024-02-11 16:35:17 -0500

    Fixed linting

--- html/components/keyboard/keyboard.php
+++ html/components/keyboard/keyboard.php
@@ -11,19 +11,19 @@
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8" />
-   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <style>
-      <?php require 'hangman_game.css'; ?>
-   </style>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+        <?php require 'hangman_game.css'; ?>
+    </style>
 </head>
 
 <div id="keyboard">
-   <?php
-      $alphabet = range('A', 'Z');
-      foreach ($alphabet as $letter) {
-         echo '<button class="key">' . $letter . '</button>';
-      }
-   ?>
+    <?php
+    $alphabet = range('A', 'Z');
+    foreach ($alphabet as $letter) {
+        echo '<button class="key">' . $letter . '</button>';
+    }
+    ?>
 </div>
