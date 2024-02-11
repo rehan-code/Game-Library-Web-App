39bde68fcecf9869b337474ff987e88ae738462b
rnagoorm <rnagoorm@uoguelph.ca>
2024-02-11 14:58:24 -0500

    completed life logic

--- html/hangman/stages/hangman_game.js
+++ html/hangman/stages/hangman_game.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     const word = "algorithm".toUpperCase();
+    let lives = 0;
     let displayedWord = Array.from(word, () => '_');
     updateDisplay();
 
+    // Add a event listener for every key click
     document.querySelectorAll('.key').forEach(function(key) {
         key.addEventListener('click', function() {
             const letter = this.textContent.toUpperCase();
-            if (word.includes(letter)) {
+            
+            if (word.includes(letter)) { // letter is correct
                 this.classList.add('correct');
                 isCorrectLetter(letter);
-            } else {
+            } else { // letter is incorrect
                 this.classList.add('incorrect');
                 isWrongLetter(letter);
             }
-            this.disabled = true;
+            this.disabled = true; //disable letter clickability
         });
     });
 
+    /**
+     * Updates the correct letter on the displayed word
+     * @param {*} letter 
+     */
     function isCorrectLetter(letter) {
         word.split('').forEach((char, index) => {
             if (char === letter) {
@@ -24,14 +31,38 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
         updateDisplay();
+        
+        // if word is complete display game over
+        if (!displayedWord.includes('_')) {
+            document.querySelector(".game-over-screen h1").innerHTML = "Congratulations! </br> You're a Winner!"
+            gameover();
+        }
     }
 
-    function isWrongLetter(letter) {
-        console.log(letter + " is incorrect");
-
-        // Add functionality here
+    /**
+     * updates the hangman figure
+     */
+    function isWrongLetter() {
+        lives++;
+        var hangman = document.querySelector(".image-container img");
+        lives < 10 ? hangman.src="../../images/hangman/hangman"+lives+".png" : null; // update hangman image
+        lives == 1 ? hangman.style.display = "block" : null; // show hangman on first life lost
+        if (lives == 9) {
+            gameover();
+        }
     }
 
+    /**
+     * function to show the game over page
+     */
+    function gameover() {
+        document.querySelector(".game-over-screen").classList.add("active");
+        document.querySelector(".game-content").classList.add("blur");
+    }
+
+    /**
+     * Update the displayed letter/underscores on the screen
+     */
     function updateDisplay() {
         document.getElementById('word-display').textContent = displayedWord.join(' ');
     }
--- html/hangman/stages/hangman_stage_1.php
+++ html/hangman/stages/hangman_stage_1.php
@@ -24,62 +24,64 @@
 <body>
     <?php require "../../components/navbar/navbar.php"; ?>
 
-    <div class="main-heading">
-        <h1>Guess the CS word!</h1>
-    </div>
-
     <div class="game-over-screen">
         <h1>Game Over</h1>
         <div class="options">
             <button class="option-button button1" 
-            onclick="window.location.href='../../index.php'"></button>
+             onclick="window.location.href='../../index.php'"></button>
             <button class="option-button button2" 
-            onclick="window.location.href='hangman_stage_1.php'"></button>
-        </div>
-    </div>
-    
-    <div class="content-container">
-        <div class="image-container">
-            <img src="../../images/hangman/hangman9.png" draggable="false"
-            alt="Zoomable Image" id="hangmanImage">
+             onclick="window.location.href='hangman_stage_1.php'"></button>
         </div>
     </div>
 
-    <div class="word-container">
-        <h2 id="word-display"></h2>
+    <div class="game-content">
+
+        <div class="main-heading">
+            <h1>Guess the CS word!</h1>
+        </div>
+
+        <div class="content-container">
+            <div class="image-container">
+                <img src="../../images/hangman/hangman1.png" 
+                 draggable="false" alt="Zoomable Image" id="hangmanImage">
+            </div>
+        </div>
+
+        <div class="word-container">
+            <h2 id="word-display"></h2>
+        </div>
+
+        <div id="keyboard">
+            <button class="key">A</button>
+            <button class="key">B</button>
+            <button class="key">C</button>
+            <button class="key">D</button>
+            <button class="key">E</button>
+            <button class="key">F</button>
+            <button class="key">G</button>
+            <button class="key">H</button>
+            <button class="key">I</button>
+            <button class="key">J</button>
+            <button class="key">K</button>
+            <button class="key">L</button>
+            <button class="key">M</button>
+            <button class="key">N</button>
+            <button class="key">O</button>
+            <button class="key">P</button>
+            <button class="key">Q</button>
+            <button class="key">R</button>
+            <button class="key">S</button>
+            <button class="key">T</button>
+            <button class="key">U</button>
+            <button class="key">V</button>
+            <button class="key">W</button>
+            <button class="key">X</button>
+            <button class="key">Y</button>
+            <button class="key">Z</button>
+        </div>
+
     </div>
-
-    <div id="keyboard">
-    <button class="key">A</button>
-    <button class="key">B</button>
-    <button class="key">C</button>
-    <button class="key">D</button>
-    <button class="key">E</button>
-    <button class="key">F</button>
-    <button class="key">G</button>
-    <button class="key">H</button>
-    <button class="key">I</button>
-    <button class="key">J</button>
-    <button class="key">K</button>
-    <button class="key">L</button>
-    <button class="key">M</button>
-    <button class="key">N</button>
-    <button class="key">O</button>
-    <button class="key">P</button>
-    <button class="key">Q</button>
-    <button class="key">R</button>
-    <button class="key">S</button>
-    <button class="key">T</button>
-    <button class="key">U</button>
-    <button class="key">V</button>
-    <button class="key">W</button>
-    <button class="key">X</button>
-    <button class="key">Y</button>
-    <button class="key">Z</button>
-</div>
-
 
 </body>
 
 </html>
-
