52a3199eed0875899c67b2d02c33fec3ac7e2ec9
Harir <57466601+Harir99@users.noreply.github.com>
2024-03-13 23:41:28 -0400

    Update cryptogram.test.js

--- html/test/cryptogram.test.js
+++ html/test/cryptogram.test.js
@@ -1,7 +1,13 @@
+/**
+ * Javascript test cases
+ * php version 8.1.2
+ * Authors: Rehan Nagoor Mohideen, Ivan Odiel Magtangob, Harir Al-Rubaye,
+ *          Harikrishan Singh, Nour Tayem, Thulasi Jothiravi
+ */
+
 import { setupInputHandlers, generateCryptogram } from '../cryptogram/game/cryptogram.js';
 
 test('setupInputHandlers - all inputs with same data-encrypted value update together', () => {
-    // Setup mock DOM
     document.body.innerHTML = `
       <div id="cryptogram">
         <div class="letter-container" data-encrypted="A">
@@ -13,23 +19,21 @@ test('setupInputHandlers - all inputs with same data-encrypted value update toge
       </div>
     `;
   
-    // Call function to setup handlers
+    // Call the function to setup input handlers
     setupInputHandlers();
   
-    // Simulate input on the first input element
+    // Set the value of the first input to 'B'
     const inputElements = document.querySelectorAll('#cryptogram .letter-container input');
     inputElements[0].value = 'B';
     inputElements[0].dispatchEvent(new Event('input'));
   
-    // Assert that the second input's value is also 'B'
+    // Assert that the value of the second input is also 'B'
     expect(inputElements[1].value).toBe('B');
   });
   
   test('generateCryptogram generates correct HTML structure', () => {
-    // Setup mock DOM
     document.body.innerHTML = `<div id="cryptogram"></div>`;
-  
-    // Provide a test sentence and letter mapping
+
     const testSentence = 'AB';
     const testMapping = { A: 'X', B: 'Y' };
   
