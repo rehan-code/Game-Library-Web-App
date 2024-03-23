e6a3290d4a717feeb14633ff1d49bed2c935aaa4
Nour Tayem <ntayem@uoguelph.ca>
2024-03-23 16:06:51 -0400

    updating Ispot game

--- html/instruction/instruction.php
+++ html/instruction/instruction.php
@@ -31,70 +31,7 @@
                 <div class="overlay1"></div>
                 <h1 class="about-main-heading1">INSTRUCTIONS</h1>
             </div>
-            <div class="game2">
-                <h2 class="heading">'I-Spot'</h2>
-                <h2 class="heading">Overview</h2>
-                <p class="heading-paragraph">
-                    Our Game, is inspired by the concept of 'Where's Waldo?' by
-                    British illustrator Martin Handford, presents a fresh twist
-                    on the classic search-and-find adventure. In these vibrant
-                    scenes, bustling with an array of unique icons, objects,
-                    and characters engaged in diverse activities, the challenge
-                    remains the same: locating our elusive icon among the crowd.
-                    Much like Waldo, our distinctive figure stands out with its
-                    own quirky characteristics amidst the intricately illustrated
-                    landscape. Get ready to embark on a captivating journey of
-                    observation and discovery as you hunt for our signature icon
-                    within each captivating image!
-                </p>
-                <h2 class="heading">Objective</h2>
-                <p class="heading-paragraph">
-                    Within each scene:
-                <ul class="list-class center-text">
-                    <li>
-                        <strong>Scene 1:</strong>
-                        Find the Lion in the Forest.
-                    </li>
-                    <li>
-                        <strong>Scene 2:</strong>
-                        Find the Dragon in the Kingdom.
-                    </li>
-                    <li>
-                        <strong>Scene 3:</strong>
-                        Find The Skull Shirt Girl in the Neighbourhood.
-                    </li>
-                </ul>
-                </p>
-
-                <h2 class="heading">Instructions</h2>
-                <div class="bottom-margin-setting">
-                    <div class="rounded-box">
-                        <p>
-                        <dl class="list-class">
-                            <li> Click on the hidden item using the
-                                <strong>Mouse</strong> button upon locating the item
-                            </li>
-                            <li> Double-click to <strong>zoom in</strong></li>
-                            <li> Failing to click the hidden item deducts
-                                <strong>1 point</strong> from your score
-                            </li>
-                            <li> Losing all points results in a
-                                <strong>Game Over</strong>
-                            </li>
-                            <li>Press the light bulb-shaped 
-                                <strong>Hint Button</strong> 
-                            for a clue about the hidden item.
-                            </li>
-                        </dl>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="bottom-margin-setting">
-                <div class="heading3">
-                    <h2></h2>
-                </div>
-            </div>
+           
             <div class="game2">
                 <h2 class="heading">'HangMan'</h2>
                 <h2 class="heading">Overview</h2>
